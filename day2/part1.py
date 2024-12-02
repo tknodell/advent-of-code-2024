75367b5e1f11c20bde85c88b32cab73fe6e89c73
Tyler Knodell <tknodell@recurly.com>
2024-12-01 23:19:23 -0700

    day 2 broken

---
+++ day2/part1.py
@@ -0,0 +1,35 @@
+
+numSafe = 0
+
+def IncreaseAndDecrease(arr):
+    increasing = all(arr[i] < arr[i + 1] for i in range(len(arr) - 1))
+    decreasing = all(arr[i] > arr[i + 1] for i in range(len(arr) - 1))
+
+    print(arr)
+    print(increasing, decreasing)
+    if increasing and decreasing:
+        return True
+
+with open('input', 'r') as file:
+    for line in file:
+        line = line.strip()
+        parts = line.split(' ')
+
+        safe = True
+
+        if IncreaseAndDecrease(parts):
+            safe = False
+            print ("not safe increasing and decreasing")
+            print(parts)
+
+        for i in range(len(parts)-1):
+            diff = abs(int(parts[i+1]) - int(parts[i]))
+            print(diff)
+            if diff > 3 or diff < 1:
+                print("not safe")
+                safe = False
+
+        if safe:
+            numSafe = numSafe + 1
+        print("---")
+        print(numSafe)
