0639277b85739a4bef998082d1b0b3539817edf0
Tyler Knodell <tknodell@recurly.com>
2024-12-11 18:50:41 -0700

    works with example

--- day11/part1.py
+++ day11/part1.py
@@ -3,25 +3,17 @@ def evenNumDigits(num):
     digits = len(number_str)
     return digits % 2 == 0
 
-def split_digits(number):
-    num_str = str(number)
-    length = len(num_str)
-    midpoint = length // 2
-
-    first_half = num_str[:midpoint + (length % 2)]
-    second_half = num_str[midpoint + (length % 2):]
-
-    return first_half, second_half
-
 def blink(stones):
     updatedStones = []
     for stone in stones:
         if stone == 0:
             updatedStones.append(1)
-        elif evenNumDigits(stone):
-            res_first, res_second = split_digits(stone)
-            updatedStones.append(res_first)
-            updatedStones.append(res_second)
+        elif len(str(stone)) % 2 == 0:
+            n = len(str(stone)) // 2
+            a = int("".join(list(str(stone))[:n]))
+            b = int("".join(list(str(stone))[n:]))
+            updatedStones.append(a)
+            updatedStones.append(b)
         else:
             updatedStones.append(stone * 2024)
     return updatedStones
@@ -34,5 +26,10 @@ stones = [int(n) for n in input.split()]
 
 print(stones)
 
-for i in range(1,6):
-    print(blink(stones))
+print(blink(stones))
+
+for i in range(6):
+    stones = blink(stones)
+    print(stones)
+
+# # print(stones)
