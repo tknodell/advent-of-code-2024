09c432ad918f76a0152aada6d6ceeb9611d886d4
Tyler Knodell <tknodell@recurly.com>
2024-12-13 12:51:22 -0700

    ugly but works

--- day13/part1.py
+++ day13/part1.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def parse_line(line):
+    #Button A: X+62, Y+27
+    x,y = line.split()[-2:]
+    return int(x[2:-1]),int(y[2:])
 
 def solveLinearMatrix(aX, aY, bX, bY, prizeX, prizeY):
     A = np.array([[aX, bX], [aY, bY]])
@@ -14,11 +18,9 @@ def solveLinearMatrix(aX, aY, bX, bY, prizeX, prizeY):
     # return np.dot(invA, B)
     # return np.linalg.solve(A,B)
 
-def calcTokens():
-    # res = print(solveLinearMatrix(26,66,67,21,12748,12176))
-    res = solveLinearMatrix(94,34,22,67,8400,5400)
-    res = np.ndarray.round(res)
-
+def calcTokens(arr):
+    res = solveLinearMatrix(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5])
+    res = np.ndarray.round(res, 4)
 
     isValid = res[0].is_integer() and res[1].is_integer()
     if isValid:
@@ -28,4 +30,8 @@ def calcTokens():
     else:
         return 0
 
-print(calcTokens)
+print(calcTokens([94,34,22,67,8400,5400]))
+print(calcTokens([26,66,67,21,12748,12176]))
+
+# with open('example') as file:
+#     D = file.read()
